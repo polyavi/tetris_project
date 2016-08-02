8acf1632db5f0841bc53664be73923826055dcdc
Todor Arabadzhiev <todor_ia@abv.bg>
2016-08-01 22:22:49 -0700

    block stops at the bottom of the field
    Some bugs fixed;
    Functionality added:
    block cannot get out of the field;
    block stops at the bottom of field and becomes part of it - new block is created and starts from the top

--- Tetris-game/js/tetris_game.js
+++ Tetris-game/js/tetris_game.js
@@ -1,8 +1,51 @@
 /* globals window document console */
 "use strict";
 
-const blocks = [
-    {
+const startGameField = {
+        "shape": [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                  [0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1],
+                  [0,0,1,1,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,1]],
+         "color": "grey"
+      },
+      blocks = [
+      {
         "shapes": [[[1,1,1],
                     [0,1,0]],
                    [[0,1],
@@ -15,20 +58,20 @@ const blocks = [
                     [1,0]]],
         "color": "red",
         "state": 0
-    },
-    {
+      },
+      {
         "shapes": [[[1,1],
-                   [1,0]],
-                  [[1,1],
-                   [0,1]],
-                  [[0,1],
-                   [1,1]],
-                  [[1,0],
-                   [1,1]]],
+                    [1,0]],
+                   [[1,1],
+                    [0,1]],
+                   [[0,1],
+                    [1,1]],
+                   [[1,0],
+                    [1,1]]],
         "color": "blue",
         "state": 0
-    }
-];
+      }
+      ];
 
 function createGame(tetrisSelector) {
    
@@ -37,27 +80,54 @@ function createGame(tetrisSelector) {
          step = buildBlockSize,
          blockLineWidth = 2,
          blockLineColor = "green",
-         maxWidth = 20 * buildBlockSize,
-         maxHeight = 40 * buildBlockSize,
-         enterPosition = {
-            "left": 10 * buildBlockSize,
-            "top": 0
-          };
+         emptyFieldColor = "white",
+         fieldWidth = startGameField.shape[0].length,
+         fieldHeight = startGameField.shape.length,
+         enterFieldLeft = 10,
+         enterFieldTop = 0;
           
     var tetrisCanvas = document.querySelector(tetrisSelector),
         ctxTetris = tetrisCanvas.getContext("2d"),
         gotToBottom = false,
         tetrisBlock = getRandomBlock(),
-        currentPosition = enterPosition,
-        delay = 2000;
+        currentFieldPosition = {
+            "left": enterFieldLeft,
+            "top": enterFieldTop
+        },
+        speed = 1000,
+        gameField = startGameField;
         
+    function drawGameFieldBlocks(field) {
+        for (var i = 0; i < field.shape.length; i+=1) {
+            for (var j = 0; j < field.shape[0].length; j+=1) {
+                if (field.shape[i][j] === 1) {
+                    drawSingleBlock({
+                        "left": j,
+                        "top": i,
+                        "size": buildBlockSize,
+                        "color": field.color,
+                        "lineColor": blockLineColor
+                    });
+                } else {
+                    drawSingleBlock({
+                        "left": j,
+                        "top": i,
+                        "size": buildBlockSize,
+                        "color": emptyFieldColor,
+                        "lineColor": blockLineColor
+                    });
+                }
+            }
+        }
+    }
+    
     function drawSingleBlock(block) {
         ctxTetris.fillStyle = block.color;
         ctxTetris.strokeStyle = block.lineColor;
         ctxTetris.lineWidth = blockLineWidth;
         ctxTetris.beginPath();
-        ctxTetris.fillRect(block.left, block.top, block.size, block.size);
-        ctxTetris.strokeRect(block.left, block.top, block.size, block.size);
+        ctxTetris.fillRect(block.left * block.size, block.top * block.size, block.size, block.size);
+        ctxTetris.strokeRect(block.left * block.size, block.top * block.size, block.size, block.size);
     }
     
     function drawTetrisBlock(pattern, position) {
@@ -65,19 +135,18 @@ function createGame(tetrisSelector) {
             vLen = shape.length,
             hLen,
             v,
-            h,
-            len;
+            h;
             
         for (v = 0; v < vLen; v+=1) {
             hLen = shape[v].length;
             for (h = 0; h < hLen; h+=1) {
                 if (shape[v][h] === 1) {
                     drawSingleBlock({
-                    "left": position.left + h * buildBlockSize,
-                    "top": position.top + v * buildBlockSize,
-                    "size": buildBlockSize,
-                    "color": pattern.color,
-                    "lineColor": blockLineColor
+                        "left": position.left + h,
+                        "top": position.top + v,
+                        "size": buildBlockSize,
+                        "color": pattern.color,
+                        "lineColor": blockLineColor
                     });
                 }
             }
@@ -90,61 +159,131 @@ function createGame(tetrisSelector) {
         return randBlock;
     }
     
+    function isValidBlockPosition() {
+        var currShape = tetrisBlock.shapes[tetrisBlock.state],
+            vLen = currShape.length,
+            hLen,
+            v,
+            h;
+
+        //Check for LEFT boundary
+        if (currentFieldPosition.left < 0) {
+            return false;
+        }
+        //Check for RIGHT boundary
+        if (currentFieldPosition.left + currShape[0].length > fieldWidth) {
+            return false;
+        }
+        //Check for BOTTOM boundary
+        if (currentFieldPosition.top + currShape.length > fieldHeight) {
+            return false;
+        }
+
+        //Check for COLLISION inside the field
+        for (v = 0; v < vLen; v+=1) {
+            hLen = currShape[v].length;
+            for (h = 0; h < hLen; h+=1) {
+                if (currShape[v][h] === 1 &&
+                    gameField.shape[currentFieldPosition.top + v][currentFieldPosition.left + h] === 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    
     function respondToKeyDown(ev) {
-        console.log(ev.keyCode);
         switch (ev.keyCode) {
             case 37: {//left
-                if (currentPosition.left > 0) {
-                    currentPosition.left -= step;
+                currentFieldPosition.left -= 1;
+                if (!isValidBlockPosition()) {
+                    currentFieldPosition.left += 1;
                 }
                 break;
             }
             case 39:{//right
-                var maxLeftPosition = maxWidth - tetrisBlock.shapes[tetrisBlock.state][0].length * buildBlockSize;
-                if (currentPosition.left < maxLeftPosition) {
-                    currentPosition.left += step;
-                } 
+                currentFieldPosition.left += 1;
+                if (!isValidBlockPosition()) {
+                    currentFieldPosition.left -= 1;
+                }
                 break;
             }
-            case 38://up: rotate clockwise
+            case 38:{//up: rotate clockwise
+                var prevState = tetrisBlock.state;
                 tetrisBlock.state = (tetrisBlock.state + 1) % blockDirections;
+                if (!isValidBlockPosition()) {
+                    tetrisBlock.state = prevState;
+                }
                 break;
-            case 40://down: rotate counter-clockwise
+            }
+            case 40:{//down: rotate counter-clockwise
+                var prevState = tetrisBlock.state;
                 tetrisBlock.state === 0 ? tetrisBlock.state = blockDirections - 1 : 
                                           tetrisBlock.state -= 1;
+                if (!isValidBlockPosition()) {
+                    tetrisBlock.state = prevState;
+                }
                 break;
+            }
             case 32://interval - position block to bottom
                 //TO DO: POSITION BLOCK TO BOTTOM
+                moveBlockDown();
                 break;
             default:
                 break;
-        }
+            }
     }
     
     function moveBlockDown() {
-        currentPosition.top += step;
+        currentFieldPosition.top += 1;
+        if (!isValidBlockPosition()) {
+            currentFieldPosition.top -= 1;
+            updateGameFieldWithBlock();
+            gotToBottom = true;
+        }
+    }
+    
+    function updateGameFieldWithBlock() {
+        var currShape = tetrisBlock.shapes[tetrisBlock.state],
+            vLen = currShape.length,
+            hLen,
+            v,
+            h;
+            
+        for (v = 0; v < vLen; v+=1) {
+            hLen = currShape[v].length;
+            for (h = 0; h < hLen; h+=1) {
+                if (currShape[v][h] === 1) {
+                    gameField.shape[currentFieldPosition.top + v][currentFieldPosition.left + h] = 1;
+                }
+            }
+        }
+        
     }
     
     function gameLoop() {
-        if (gotToBottom) {
-            ctxTetris.clearRect(0,0, tetrisCanvas.clientWidth, tetrisCanvas.clientHeight);
-            tetrisBlock = getRandomBlock();
-            currentPosition = enterPosition;
-            gotToBottom = false;
-        } else {
-            //clear only BLOCK - TO DO!!!!
-            ctxTetris.clearRect(0,0, tetrisCanvas.clientWidth, tetrisCanvas.clientHeight);
-        }
         
-        drawTetrisBlock(tetrisBlock, currentPosition);
+        if (gotToBottom) {
+            tetrisBlock = getRandomBlock();
+            currentFieldPosition = {
+                "left": enterFieldLeft,
+                "top": enterFieldTop
+            };
+            gotToBottom = false;
+        };
+        
+        ctxTetris.clearRect(0,0, tetrisCanvas.clientWidth, tetrisCanvas.clientHeight);
+        drawGameFieldBlocks(gameField);
+        drawTetrisBlock(tetrisBlock, currentFieldPosition);
         
         window.requestAnimationFrame(gameLoop);
     }
-    
-    ctxTetris.canvas.width = maxWidth;
-    ctxTetris.canvas.height = maxHeight;    
+
+    ctxTetris.canvas.width = fieldWidth * buildBlockSize;
+    ctxTetris.canvas.height = fieldHeight * buildBlockSize;
     document.body.addEventListener("keydown", respondToKeyDown);
-    setInterval(moveBlockDown, delay);
+    setInterval(moveBlockDown, speed);
      
     return {
         "start": function() {
