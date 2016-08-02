622ad1ef6a4015f99a234b163e399c19e3a43370
YavorYanchev <yavor_yanchev_bs@abv.bg>
2016-08-02 11:22:36 +0300

    Added sounds to left and right

--- Tetris-game/js/tetris_game.js
+++ Tetris-game/js/tetris_game.js
@@ -2,90 +2,90 @@
 "use strict";
 
 const startGameField = {
-        "shape": [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                  [0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1],
-                  [0,0,1,1,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,1]],
-         "color": "grey"
-      },
-      blocks = [
-      {
-        "shapes": [[[1,1,1],
-                    [0,1,0]],
-                   [[0,1],
-                    [1,1],
-                    [0,1]],
-                   [[0,1,0],
-                    [1,1,1]],
-                   [[1,0],
-                    [1,1],
-                    [1,0]]],
-        "color": "red",
-        "state": 0
-      },
-      {
-        "shapes": [[[1,1],
-                    [1,0]],
-                   [[1,1],
-                    [0,1]],
-                   [[0,1],
-                    [1,1]],
-                   [[1,0],
-                    [1,1]]],
-        "color": "blue",
-        "state": 0
-      }
-      ];
+    "shape": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1]],
+    "color": "grey"
+},
+    blocks = [
+        {
+            "shapes": [[[1, 1, 1],
+                [0, 1, 0]],
+                [[0, 1],
+                    [1, 1],
+                    [0, 1]],
+                [[0, 1, 0],
+                    [1, 1, 1]],
+                [[1, 0],
+                    [1, 1],
+                    [1, 0]]],
+            "color": "red",
+            "state": 0
+        },
+        {
+            "shapes": [[[1, 1],
+                [1, 0]],
+                [[1, 1],
+                    [0, 1]],
+                [[0, 1],
+                    [1, 1]],
+                [[1, 0],
+                    [1, 1]]],
+            "color": "blue",
+            "state": 0
+        }
+    ];
 
 function createGame(tetrisSelector) {
-   
-   const buildBlockSize = 15,
-         blockDirections = 4,
-         step = buildBlockSize,
-         blockLineWidth = 2,
-         blockLineColor = "green",
-         emptyFieldColor = "white",
-         fieldWidth = startGameField.shape[0].length,
-         fieldHeight = startGameField.shape.length,
-         enterFieldLeft = 10,
-         enterFieldTop = 0;
-          
+
+    const buildBlockSize = 15,
+        blockDirections = 4,
+        step = buildBlockSize,
+        blockLineWidth = 2,
+        blockLineColor = "green",
+        emptyFieldColor = "white",
+        fieldWidth = startGameField.shape[0].length,
+        fieldHeight = startGameField.shape.length,
+        enterFieldLeft = 10,
+        enterFieldTop = 0;
+
     var tetrisCanvas = document.querySelector(tetrisSelector),
         ctxTetris = tetrisCanvas.getContext("2d"),
         gotToBottom = false,
@@ -96,10 +96,10 @@ function createGame(tetrisSelector) {
         },
         speed = 1000,
         gameField = startGameField;
-        
+
     function drawGameFieldBlocks(field) {
-        for (var i = 0; i < field.shape.length; i+=1) {
-            for (var j = 0; j < field.shape[0].length; j+=1) {
+        for (var i = 0; i < field.shape.length; i += 1) {
+            for (var j = 0; j < field.shape[0].length; j += 1) {
                 if (field.shape[i][j] === 1) {
                     drawSingleBlock({
                         "left": j,
@@ -120,7 +120,7 @@ function createGame(tetrisSelector) {
             }
         }
     }
-    
+
     function drawSingleBlock(block) {
         ctxTetris.fillStyle = block.color;
         ctxTetris.strokeStyle = block.lineColor;
@@ -129,17 +129,17 @@ function createGame(tetrisSelector) {
         ctxTetris.fillRect(block.left * block.size, block.top * block.size, block.size, block.size);
         ctxTetris.strokeRect(block.left * block.size, block.top * block.size, block.size, block.size);
     }
-    
+
     function drawTetrisBlock(pattern, position) {
         var shape = pattern.shapes[pattern.state],
             vLen = shape.length,
             hLen,
             v,
             h;
-            
-        for (v = 0; v < vLen; v+=1) {
+
+        for (v = 0; v < vLen; v += 1) {
             hLen = shape[v].length;
-            for (h = 0; h < hLen; h+=1) {
+            for (h = 0; h < hLen; h += 1) {
                 if (shape[v][h] === 1) {
                     drawSingleBlock({
                         "left": position.left + h,
@@ -152,13 +152,13 @@ function createGame(tetrisSelector) {
             }
         }
     }
-    
+
     function getRandomBlock() {
         var randBlock = blocks[Math.floor(Math.random() * blocks.length)];
         randBlock.state = Math.floor(Math.random() * blockDirections);
         return randBlock;
     }
-    
+
     function isValidBlockPosition() {
         var currShape = tetrisBlock.shapes[tetrisBlock.state],
             vLen = currShape.length,
@@ -180,9 +180,9 @@ function createGame(tetrisSelector) {
         }
 
         //Check for COLLISION inside the field
-        for (v = 0; v < vLen; v+=1) {
+        for (v = 0; v < vLen; v += 1) {
             hLen = currShape[v].length;
-            for (h = 0; h < hLen; h+=1) {
+            for (h = 0; h < hLen; h += 1) {
                 if (currShape[v][h] === 1 &&
                     gameField.shape[currentFieldPosition.top + v][currentFieldPosition.left + h] === 1) {
                     return false;
@@ -192,24 +192,27 @@ function createGame(tetrisSelector) {
 
         return true;
     }
-    
+
     function respondToKeyDown(ev) {
         switch (ev.keyCode) {
             case 37: {//left
+                //play sound
+                document.getElementById('moveLeftRight').play();
                 currentFieldPosition.left -= 1;
                 if (!isValidBlockPosition()) {
                     currentFieldPosition.left += 1;
                 }
                 break;
             }
-            case 39:{//right
+            case 39: {//right
+                document.getElementById('moveLeftRight').play();
                 currentFieldPosition.left += 1;
                 if (!isValidBlockPosition()) {
                     currentFieldPosition.left -= 1;
                 }
                 break;
             }
-            case 38:{//up: rotate clockwise
+            case 38: {//up: rotate clockwise
                 var prevState = tetrisBlock.state;
                 tetrisBlock.state = (tetrisBlock.state + 1) % blockDirections;
                 if (!isValidBlockPosition()) {
@@ -217,10 +220,10 @@ function createGame(tetrisSelector) {
                 }
                 break;
             }
-            case 40:{//down: rotate counter-clockwise
+            case 40: {//down: rotate counter-clockwise
                 var prevState = tetrisBlock.state;
-                tetrisBlock.state === 0 ? tetrisBlock.state = blockDirections - 1 : 
-                                          tetrisBlock.state -= 1;
+                tetrisBlock.state === 0 ? tetrisBlock.state = blockDirections - 1 :
+                    tetrisBlock.state -= 1;
                 if (!isValidBlockPosition()) {
                     tetrisBlock.state = prevState;
                 }
@@ -232,9 +235,9 @@ function createGame(tetrisSelector) {
                 break;
             default:
                 break;
-            }
+        }
     }
-    
+
     function moveBlockDown() {
         currentFieldPosition.top += 1;
         if (!isValidBlockPosition()) {
@@ -243,27 +246,27 @@ function createGame(tetrisSelector) {
             gotToBottom = true;
         }
     }
-    
+
     function updateGameFieldWithBlock() {
         var currShape = tetrisBlock.shapes[tetrisBlock.state],
             vLen = currShape.length,
             hLen,
             v,
             h;
-            
-        for (v = 0; v < vLen; v+=1) {
+
+        for (v = 0; v < vLen; v += 1) {
             hLen = currShape[v].length;
-            for (h = 0; h < hLen; h+=1) {
+            for (h = 0; h < hLen; h += 1) {
                 if (currShape[v][h] === 1) {
                     gameField.shape[currentFieldPosition.top + v][currentFieldPosition.left + h] = 1;
                 }
             }
         }
-        
+
     }
-    
+
     function gameLoop() {
-        
+
         if (gotToBottom) {
             tetrisBlock = getRandomBlock();
             currentFieldPosition = {
@@ -272,11 +275,11 @@ function createGame(tetrisSelector) {
             };
             gotToBottom = false;
         };
-        
-        ctxTetris.clearRect(0,0, tetrisCanvas.clientWidth, tetrisCanvas.clientHeight);
+
+        ctxTetris.clearRect(0, 0, tetrisCanvas.clientWidth, tetrisCanvas.clientHeight);
         drawGameFieldBlocks(gameField);
         drawTetrisBlock(tetrisBlock, currentFieldPosition);
-        
+
         window.requestAnimationFrame(gameLoop);
     }
 
@@ -284,9 +287,9 @@ function createGame(tetrisSelector) {
     ctxTetris.canvas.height = fieldHeight * buildBlockSize;
     document.body.addEventListener("keydown", respondToKeyDown);
     setInterval(moveBlockDown, speed);
-     
+
     return {
-        "start": function() {
+        "start": function () {
             gameLoop();
         }
     };
