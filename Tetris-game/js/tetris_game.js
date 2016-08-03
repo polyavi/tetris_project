863da0d458ad0d854de7dc660b2084aa75648829
Polya Ivanova <polia.v.ivanova@gmail.com>
2016-08-03 14:01:00 +0300

    styles

--- Tetris-game/js/tetris_game.js
+++ Tetris-game/js/tetris_game.js
@@ -2,158 +2,227 @@
 "use strict";
 
 const startGameField = {
-    "shape": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1]],
-    "color": "grey"
-},
-    blocks = [
-        {
-            "shapes": [[[1, 1, 1],
-                [0, 1, 0]],
-                [[0, 1],
-                    [1, 1],
-                    [0, 1]],
-                [[0, 1, 0],
-                    [1, 1, 1]],
-                [[1, 0],
-                    [1, 1],
-                    [1, 0]]],
-            "color": "red",
-            "state": 0
-        },
-        {
-            "shapes": [[[1, 1],
-                [1, 0]],
-                [[1, 1],
-                    [0, 1]],
-                [[0, 1],
-                    [1, 1]],
-                [[1, 0],
-                    [1, 1]]],
-            "color": "blue",
-            "state": 0
-        },
-         { //cube
-            "shapes": [[[1, 1],
-                [1, 1]],
-                [[1, 1],
-                    [1, 1]],
-                [[1, 1],
-                    [1, 1]],
-                [[1, 1],
-                    [1, 1]]],
-            "color": "purple",
-            "state": 0
-        },
-        {//stretched cube #1 or s-shaped
-            "shapes": [[[1, 0],
-                [1, 1],
-                [0, 1]],
-                [[0, 1, 1],
-                    [1, 1, 0]],
-                [[1, 0],
-                    [1, 1],
-                    [0, 1]],
-                [[0, 1, 1],
-                    [1, 1, 0]]],
-            "color": "#EA3B28",
-            "state": 0
-        },
-        {//stretched cube #2 or s-shaped
-            "shapes": [[[0, 1],
-                [1, 1],
-                [1, 0]],
-                [[1, 1, 0],
-                    [0, 1, 1]],
-                [[0, 1],
-                    [1, 1],
-                    [1, 0]],
-                [[1, 1, 0],
-                    [0, 1, 1]]],
-            "color": "#FEBA17",
-            "state": 0
-        },
-        {//bulgarian Г-shaped #1
-            "shapes": [[[1, 1],
-                [1, 0],
-                [1, 0]],
-                [[1, 1, 1],
-                    [0, 0, 1]],
-                [[0, 1],
-                    [0, 1],
-                    [1, 1]],
-                [[1, 0, 0],
-                    [1, 1, 1]]],
-            "color": "#2BFAAA",
-            "state": 0
-        },
-        {//bulgarian Г-shaped inverted
-            "shapes": [[[1, 1],
+        "shape": [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        ],
+        "color": "rgb(56, 92, 122)"
+    },
+    blocks = [{
+        "shapes": [
+            [
+                [1, 1, 1],
+                [0, 1, 0]
+            ],
+            [
                 [0, 1],
-                [0, 1]],
-                [[0, 0, 1],
-                    [1, 1, 1]],
-                [[1, 0],
-                    [1, 0],
-                    [1, 1]],
-                [[1, 1, 1],
-                    [1, 0, 0]]],
-            "color": "#0B3D84",
-            "state": 0
-        },
-        {//I-shaped
-            "shapes": [[[1],
+                [1, 1],
+                [0, 1]
+            ],
+            [
+                [0, 1, 0],
+                [1, 1, 1]
+            ],
+            [
+                [1, 0],
+                [1, 1],
+                [1, 0]
+            ]
+        ],
+        "color": "rgb(187, 35, 67)",
+        "state": 0
+    }, {
+        "shapes": [
+            [
+                [1, 1],
+                [1, 0]
+            ],
+            [
+                [1, 1],
+                [0, 1]
+            ],
+            [
+                [0, 1],
+                [1, 1]
+            ],
+            [
+                [1, 0],
+                [1, 1]
+            ]
+        ],
+        "color": "rgb(73, 90, 184)",
+        "state": 0
+    }, { //cube
+        "shapes": [
+            [
+                [1, 1],
+                [1, 1]
+            ],
+            [
+                [1, 1],
+                [1, 1]
+            ],
+            [
+                [1, 1],
+                [1, 1]
+            ],
+            [
+                [1, 1],
+                [1, 1]
+            ]
+        ],
+        "color": "rgb(153, 28, 140)",
+        "state": 0
+    }, { //stretched cube #1 or s-shaped
+        "shapes": [
+            [
+                [1, 0],
+                [1, 1],
+                [0, 1]
+            ],
+            [
+                [0, 1, 1],
+                [1, 1, 0]
+            ],
+            [
+                [1, 0],
+                [1, 1],
+                [0, 1]
+            ],
+            [
+                [0, 1, 1],
+                [1, 1, 0]
+            ]
+        ],
+        "color": "rgb(215, 122, 24)",
+        "state": 0
+    }, { //stretched cube #2 or s-shaped
+        "shapes": [
+            [
+                [0, 1],
+                [1, 1],
+                [1, 0]
+            ],
+            [
+                [1, 1, 0],
+                [0, 1, 1]
+            ],
+            [
+                [0, 1],
+                [1, 1],
+                [1, 0]
+            ],
+            [
+                [1, 1, 0],
+                [0, 1, 1]
+            ]
+        ],
+        "color": "rgb(222, 231, 61)",
+        "state": 0
+    }, { //bulgarian Г-shaped #1
+        "shapes": [
+            [
+                [1, 1],
+                [1, 0],
+                [1, 0]
+            ],
+            [
+                [1, 1, 1],
+                [0, 0, 1]
+            ],
+            [
+                [0, 1],
+                [0, 1],
+                [1, 1]
+            ],
+            [
+                [1, 0, 0],
+                [1, 1, 1]
+            ]
+        ],
+        "color": "rgb(20, 232, 124)",
+        "state": 0
+    }, { //bulgarian Г-shaped inverted
+        "shapes": [
+            [
+                [1, 1],
+                [0, 1],
+                [0, 1]
+            ],
+            [
+                [0, 0, 1],
+                [1, 1, 1]
+            ],
+            [
+                [1, 0],
+                [1, 0],
+                [1, 1]
+            ],
+            [
+                [1, 1, 1],
+                [1, 0, 0]
+            ]
+        ],
+        "color": "rgb(204, 60, 134)",
+        "state": 0
+    }, { //I-shaped
+        "shapes": [
+            [
                 [1],
                 [1],
-                [1]],
-                [[1, 1, 1, 1]],
-                [[1],
-                    [1],
-                    [1],
-                    [1]],
-                [[1, 1, 1, 1]]],
-            "color": "#57A8C7",
-            "state": 0
-        }
-    ];
+                [1],
+                [1]
+            ],
+            [
+                [1, 1, 1, 1]
+            ],
+            [
+                [1],
+                [1],
+                [1],
+                [1]
+            ],
+            [
+                [1, 1, 1, 1]
+            ]
+        ],
+        "color": "rgb(91, 187, 228)",
+        "state": 0
+    }];
 
 function createGame(tetrisSelector) {
 
@@ -161,8 +230,8 @@ function createGame(tetrisSelector) {
         blockDirections = 4,
         step = buildBlockSize,
         blockLineWidth = 2,
-        blockLineColor = "green",
-        emptyFieldColor = "white",
+        //blockLineColor = "green",
+        //emptyFieldColor = "white",
         fieldWidth = startGameField.shape[0].length,
         fieldHeight = startGameField.shape.length,
         enterFieldLeft = 10,
@@ -188,15 +257,15 @@ function createGame(tetrisSelector) {
                         "top": i,
                         "size": buildBlockSize,
                         "color": field.color,
-                        "lineColor": blockLineColor
+                        "lineColor": "rgb(26, 29, 50)"
                     });
                 } else {
                     drawSingleBlock({
                         "left": j,
                         "top": i,
                         "size": buildBlockSize,
-                        "color": emptyFieldColor,
-                        "lineColor": blockLineColor
+                        "color":"rgba(0, 0, 0, 0)",
+                        "lineColor": "rgba(0, 0, 0, 0)"
                     });
                 }
             }
@@ -228,7 +297,7 @@ function createGame(tetrisSelector) {
                         "top": position.top + v,
                         "size": buildBlockSize,
                         "color": pattern.color,
-                        "lineColor": blockLineColor
+                        "lineColor": "rgb(255, 255, 255)"
                     });
                 }
             }
@@ -277,45 +346,48 @@ function createGame(tetrisSelector) {
 
     function respondToKeyDown(ev) {
         switch (ev.keyCode) {
-            case 37: {//left
-                //play sound
-                document.getElementById('moveLeftRight').play();
-                currentFieldPosition.left -= 1;
-                if (!isValidBlockPosition()) {
-                    currentFieldPosition.left += 1;
-                }
-                break;
-            }
-            case 39: {//right
-                document.getElementById('moveLeftRight').play();
-                currentFieldPosition.left += 1;
-                if (!isValidBlockPosition()) {
+            case 37:
+                { //left
+                    //play sound
+                    document.getElementById('moveLeftRight').play();
                     currentFieldPosition.left -= 1;
+                    if (!isValidBlockPosition()) {
+                        currentFieldPosition.left += 1;
+                    }
+                    break;
                 }
-                break;
-            }
-            case 38: {//up: rotate clockwise
-                document.getElementById('rotateLeftRight').play();
+            case 39:
+                { //right
+                    document.getElementById('moveLeftRight').play();
+                    currentFieldPosition.left += 1;
+                    if (!isValidBlockPosition()) {
+                        currentFieldPosition.left -= 1;
+                    }
+                    break;
+                }
+            case 38:
+                { //up: rotate clockwise
+                    document.getElementById('rotateLeftRight').play();
 
-                var prevState = tetrisBlock.state;
-                tetrisBlock.state = (tetrisBlock.state + 1) % blockDirections;
-                if (!isValidBlockPosition()) {
-                    tetrisBlock.state = prevState;
+                    var prevState = tetrisBlock.state;
+                    tetrisBlock.state = (tetrisBlock.state + 1) % blockDirections;
+                    if (!isValidBlockPosition()) {
+                        tetrisBlock.state = prevState;
+                    }
+                    break;
                 }
-                break;
-            }
-            case 40: {//down: rotate counter-clockwise
-                document.getElementById('rotateLeftRight').play();
+            case 40:
+                { //down: rotate counter-clockwise
+                    document.getElementById('rotateLeftRight').play();
 
-                var prevState = tetrisBlock.state;
-                tetrisBlock.state === 0 ? tetrisBlock.state = blockDirections - 1 :
-                    tetrisBlock.state -= 1;
-                if (!isValidBlockPosition()) {
-                    tetrisBlock.state = prevState;
+                    var prevState = tetrisBlock.state;
+                    tetrisBlock.state === 0 ? tetrisBlock.state = blockDirections - 1 : tetrisBlock.state -= 1;
+                    if (!isValidBlockPosition()) {
+                        tetrisBlock.state = prevState;
+                    }
+                    break;
                 }
-                break;
-            }
-            case 32://interval - position block to bottom
+            case 32: //interval - position block to bottom
                 //TO DO: POSITION BLOCK TO BOTTOM
                 document.getElementById('hardDrop').play();
 
@@ -354,30 +426,31 @@ function createGame(tetrisSelector) {
         }
 
     }
-     function clearFullRows() {
-                 
-       var  vLen = startGameField.shape[0].length, //length of rows = 20
-            hLen = startGameField.shape.length,    // length of cols = 40
-            countRow=0,
+
+    function clearFullRows() {
+
+        var vLen = startGameField.shape[0].length, //length of rows = 20
+            hLen = startGameField.shape.length, // length of cols = 40
+            countRow = 0,
             h,
             v,
-            zeroArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+            zeroArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-        for (h = 0; h < hLen; h+=1) {
+        for (h = 0; h < hLen; h += 1) {
 
 
-            
-            for (v = 0; v < vLen; v+=1){
+
+            for (v = 0; v < vLen; v += 1) {
                 countRow += startGameField.shape[h][v];
             }
-                if(countRow===20)               // check if all are 1
+            if (countRow === 20) // check if all are 1
             {
-                startGameField.shape.splice(h,1);  // remove row
-                startGameField.shape.unshift(zeroArray);  // add zeroArray at front
-            }  
-                countRow = 0;     
+                startGameField.shape.splice(h, 1); // remove row
+                startGameField.shape.unshift(zeroArray); // add zeroArray at front
+            }
+            countRow = 0;
         }
-                
+
     }
 
     function gameLoop() {
@@ -404,20 +477,19 @@ function createGame(tetrisSelector) {
     setInterval(moveBlockDown, speed);
 
 
-//sounds mute
-var audio = document.getElementById('originalTheme');
+    //sounds mute
+    var audio = document.getElementById('originalTheme');
 
-document.getElementById('muteSound').addEventListener('click', function (e)
-{
-    e = e || window.event;
-    audio.muted = !audio.muted;
-    e.preventDefault();
-}, false);
+    document.getElementById('muteSound').addEventListener('click', function(e) {
+        e = e || window.event;
+        audio.muted = !audio.muted;
+        e.preventDefault();
+    }, false);
 
     return {
-        "start": function () {
+        "start": function() {
             gameLoop();
-            
+
         }
     };
 }
